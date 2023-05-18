1e4c3b2eea62e84bbc2a1a9cc15cbf3b2e1aa1ef
Pedro <pedrovilarinho.gpd@gmail.com>
2023-05-18 16:41:49 -0300

    feat font import and config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div className="h-screen bg-zinc-950 p-6 text-zinc-100">
       <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
+      <h1 className="font-alt text-4xl">Sua cápsula do tempo</h1>
     </div>
   )
 }
