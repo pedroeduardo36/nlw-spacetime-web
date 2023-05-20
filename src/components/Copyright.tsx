6fd6b7f6e1e744d77babba2e8b628e1481f61301
Pedro <pedrovilarinho.gpd@gmail.com>
2023-05-20 14:18:06 -0300

    git oauth

---
+++ src/components/Copyright.tsx
@@ -0,0 +1,15 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito com 💜 no Nlw por{' '}
+      <a
+        target="_blank"
+        rel="noreferrer"
+        className="underline underline-offset-2 transition-colors hover:text-gray-50"
+        href="https://github.com/pedroeduardo36"
+      >
+        Pedro Andrade
+      </a>
+    </div>
+  )
+}
